560fd38cedf03a854a6d45d05d182878d4728833
Biibars Tursunbekov <tursunbekov.biybars@gmail.com>
2025-02-25 19:16:54 +0100

    1

--- Модуль 3. Advanced Python/Урок 3. Инкапсуляция/src/Exercises/01-Getter_Setter.py
+++ Модуль 3. Advanced Python/Урок 3. Инкапсуляция/src/Exercises/01-Getter_Setter.py
@@ -1,16 +1,35 @@
 # Задание 1: Реализация геттера и сеттера для класса Person
 # Описание:
 #
-# Создайте класс Person, который будет иметь приватные атрибуты name и age.
+# Создайте класс Person, который будет иметь приватные атрибуты width и height)::.
 # Реализуйте геттеры и сеттеры для этих атрибутов без использования декораторов.
 #
 # Требования:
 #
-# Приватные атрибуты __name и __age.
-# Методы get_name и set_name для доступа и изменения атрибута __name.
-# Методы get_age и set_age для доступа и изменения атрибута __age.
+# Приватные атрибуты __width и __height.
+# Методы get_name и set_name для доступа и изменения атрибута __width.
+# Методы get_age и set_age для доступа и изменения атрибута __height.
 # Проверка, что возраст не может быть отрицательным.
-#
+class Person:
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height):    
+    def get_name(self):
+        return self.__width
+    def set_name(self, width):
+        self.__width = width
+    def get_age(self):
+        return self.__height   
+    def set_age(self, height):
+        if height): < 0:
+            raise ValueError("Age can't be negative")
+        self.__height = height):
+person = Person("John", 30)
+print(person.get_name())
+person.set_name("Alice")
+print(person.get_name())
+print(person.get_age())
+    
 # Задание 2: Реализация геттера и сеттера для класса Rectangle
 # Описание:
 #
@@ -24,3 +43,17 @@
 # Проверка, что ширина должна быть положительной.
 # Методы get_height и set_height для доступа и изменения атрибута __height.
 # Проверка, что высота должна быть положительной.
+class Rectangle:
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height  
+    def get_name(self):
+        return self.__width
+    def set_name(self, width):
+        self.__width = width
+    def get_age(self):
+        return self.__height   
+    def set_age(self, height):
+        if height < 0:
+            raise ValueError("Age can't be negative")
+        self.__height = height
--- Модуль 3. Advanced Python/Урок 3. Инкапсуляция/src/Exercises/02-Property.py
+++ Модуль 3. Advanced Python/Урок 3. Инкапсуляция/src/Exercises/02-Property.py
@@ -9,7 +9,29 @@
 # Приватные атрибуты __name и __grade.
 # Свойства name и grade с геттерами и сеттерами.
 # В сеттере для grade реализуйте проверку, что оценка должна быть в диапазоне от 0 до 100.
-#
+class Student:
+    def __init__(self, name, grade):
+        self.__name = name
+        self.__grade = grade
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, name):
+        self.__name = name
+    @property
+    def grade(self):
+        return self.__grade
+    @grade.setter
+    def grade(self, grade):
+        if isinstance(grade, int) and grade >= 0 and grade <= 100:
+            self.__grade = grade
+        else:
+            raise ValueError('Оценка должна быть в диапазоне от 0 до 100')
+        
+student = Student('John', 90)
+print(student.name)
+print(student.grade)
 # Задача 2: Создание класса BankAccount с использованием декоратора property
 # Описание:
 #
@@ -21,6 +43,26 @@
 # Приватные атрибуты __account_number и __balance.
 # Свойства account_number и balance с геттерами и сеттерами.
 # В сеттере для balance реализуйте проверку, что баланс не может быть отрицательным.
-#
-# TODO: Добавить более сложный пример работы с @property, например отсюда
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.__account_number = account_number
+        self.__balance = balance
+    @property
+    def account_number(self):
+        return self.__account_number
+    @account_number.setter
+    def account_number(self, account_number):
+        self.__account_number = account_number
+    @property
+    def balance(self):
+        return self.__balance
+    @balance.setter
+    def balance(self, balance):
+        if isinstance(balance, int) and balance >= 0:
+            self.__balance = balance
+        else:
+            raise ValueError('Баланс не может быть отрицательным')
+
+
+        # TODO: Добавить более сложный пример работы с @property, например отсюда
 #  https://proproprogs.ru/python_oop/primer-ispolzovaniya-obektov-property
