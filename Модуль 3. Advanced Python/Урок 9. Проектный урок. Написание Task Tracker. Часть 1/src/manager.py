9ef3133ad1c06690ab16af1987a075cf45cc3cf4
Alexandr Meshchriakov <avm@sh-inc.ru>
2025-03-10 18:40:13 +0100

    add

---
+++ Модуль 3. Advanced Python/Урок 9. Проектный урок. Написание Task Tracker. Часть 1/src/manager.py
@@ -0,0 +1,44 @@
+import json
+
+
+class Manager:
+    def __init__(self, config_file_name: str):
+        """
+        Manager constructor
+        :param config_file_name:
+        """
+
+
+    @staticmethod
+    def __load_task_list(task_list_file: str):
+        """
+        Load task list from file
+        :param task_list_file:
+        :return:
+        """
+
+    def save_task_list(self):
+        """
+        Save task list to file
+        :return:
+        """
+
+    def create_task(self, title: str):
+        """
+        Create new task
+        :param title:
+        :return:
+        """
+
+    def delete_task(self, task_id: int):
+        """
+        Delete task by id
+        :param task_id:
+        :return:
+        """
+
+    def get_task_list(self):
+        """
+        Get task list
+        :return:
+        """
---
+++ Модуль 3. Advanced Python/Урок 9. Проектный урок. Написание Task Tracker. Часть 1/src/task.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class Task:
+    """
+    This class represents a task for task management system
+    """
+
+    def __init__(self, title: str):
+        """
+        Task constructor
+        :param title:
+        """
+
+    @property
+    def title(self):
+        """
+        Get task title
+        :return:
+        """
+
+    @title.setter
+    def title(self, title):
+        """
+        Set task title
+        :param title:
+        :return:
+        """
+
+    @property
+    def description(self):
+        """
+        Get task description
+        :return:
+        """
+
+    @description.setter
+    def description(self, description):
+        """
+        Set task description
+        :param description:
+        :return:
+        """
+
+    @property
+    def deadline(self):
+        """
+        Get task deadline
+        :return:
+        """
+
+    @deadline.setter
+    def deadline(self, deadline: datetime):
+        """
+        Set task deadline
+        :param deadline:
+        :return:
+        """
+
+    @property
+    def status(self):
+        """
+        Get task status
+        :return:
+        """
+
+    @status.setter
+    def status(self, new_status: str):
+        """
+        Set task status
+        :param new_status:
+        :return:
+        """
+
+    @property
+    def id(self):
+        """
+        Get task id
+        :return:
+        """
+
+    @id.setter
+    def id(self, new_id: int):
+        """
+        Set task id
+        :param new_id:
+        :return:
+        """
